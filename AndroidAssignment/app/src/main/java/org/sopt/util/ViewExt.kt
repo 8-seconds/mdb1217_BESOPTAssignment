89647127accad5fba4b109e53c65047298fe217d
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-04-08 02:29:03 +0900

    [ADD] #2 - add SharedPreference and ViewExtension

---
+++ AndroidAssignment/app/src/main/java/org/sopt/util/ViewExt.kt
@@ -0,0 +1,8 @@
+package org.sopt.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
