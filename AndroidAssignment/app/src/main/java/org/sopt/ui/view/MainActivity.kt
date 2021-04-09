108b82f787e1199eb9d8e6c82d0e164cc7ac2f59
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-04-10 05:34:47 +0900

    [FEAT] #4 - introduce page

--- AndroidAssignment/app/src/main/java/org/sopt/ui/view/MainActivity.kt
+++ AndroidAssignment/app/src/main/java/org/sopt/ui/view/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.sopt.ui.view
 
+import android.app.Activity
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.R
 import org.sopt.databinding.ActivityMainBinding
 import org.sopt.ui.base.BaseActivity
 import org.sopt.ui.viewmodel.HomeViewModel
+import org.sopt.util.setFadeInAnim
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, HomeViewModel>() {
@@ -14,14 +17,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, HomeViewModel>() {
     override val viewModel: HomeViewModel by viewModels()
 
     override fun initView() {
-
+        setImage()
+        setAnim()
     }
 
-    override fun initBeforeBinding() {
-
+    private fun setImage() {
+        Glide.with(this).load(R.raw.gif_move_mint).circleCrop().into(binding.ivMintLogo)
+        Glide.with(this).load(R.drawable.img_dabin).circleCrop().into(binding.ivOwner)
     }
 
-    override fun initAfterBinding() {
+    private fun setAnim() {
+        binding.ivOwner.startAnimation(setFadeInAnim(R.anim.fade_in_1000))
+        binding.tvName.startAnimation(setFadeInAnim(R.anim.fade_in_1000))
+        binding.tvGit.startAnimation(setFadeInAnim(R.anim.fade_in_1000))
 
+        binding.line.startAnimation(setFadeInAnim(R.anim.fade_in_1500))
+
+        binding.tvWelcome.startAnimation(setFadeInAnim((R.anim.fade_in_2000)))
+        binding.tvIntroduce.startAnimation(setFadeInAnim((R.anim.fade_in_2000)))
     }
+    
 }
