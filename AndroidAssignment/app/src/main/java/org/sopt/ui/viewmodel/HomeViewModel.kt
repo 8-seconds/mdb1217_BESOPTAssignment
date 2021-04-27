6c20d67afc1d540e3eb3683b1e34851e41613fed
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-04-27 15:19:10 +0900

    [ADD] #13 - add repo data logic

--- AndroidAssignment/app/src/main/java/org/sopt/ui/viewmodel/HomeViewModel.kt
+++ AndroidAssignment/app/src/main/java/org/sopt/ui/viewmodel/HomeViewModel.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.sopt.data.local.entity.ProfileData
+import org.sopt.data.local.entity.RepoData
 import org.sopt.data.repository.HomeRepo
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel@Inject constructor(private val homeRepo: HomeRepo) : ViewModel() {
-    private val profileDataList = homeRepo.getProfileDataAll()
-    fun getProfileDataAll(): LiveData<List<ProfileData>> = profileDataList
+    fun getProfileDataAll(): LiveData<List<ProfileData>> = homeRepo.getProfileDataAll()
 
     fun insertProfileData(profileData: ProfileData) {
         homeRepo.insertProfileData(profileData)
@@ -18,5 +18,21 @@ class HomeViewModel@Inject constructor(private val homeRepo: HomeRepo) : ViewMod
 
     fun deleteProfileData(profileData: ProfileData) {
         homeRepo.deleteProfileData(profileData)
+    }
+
+    fun getRepoDataAll(): LiveData<List<RepoData>> = homeRepo.getRepoDataAll()
+
+    fun insertRepoData(repoData: RepoData) {
+        homeRepo.insertRepoData(repoData)
+    }
+
+    fun getStaredRepo(): LiveData<List<RepoData>> = homeRepo.getStaredRepo()
+
+    fun updateStar(isSelected : Int, id : Long) {
+        homeRepo.updateStar(isSelected, id)
+    }
+
+    fun deleteRepoData(repoData: RepoData) {
+        homeRepo.deleteRepoData(repoData)
     }
 }
